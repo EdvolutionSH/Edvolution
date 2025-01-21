29c0ec064fbc3ed76629196dc6d91703c5567546
Fernando Gonzalez <42556413+f3rglez@users.noreply.github.com>
2025-01-21 11:31:26 -0600

    update report

--- addons/reseller_module/models/reseller_subscription.py
+++ addons/reseller_module/models/reseller_subscription.py
@@ -159,41 +159,120 @@ class ResellerSubscription(models.Model):
         for col_num, header in enumerate(headers):
             worksheet.write(0, col_num, header)
 
-        visible_subscriptions = self.env['reseller.subscription'].search([('status','=', 'ACTIVE')])
-
+        visible_subscriptions = self.env['reseller.subscription'].search([
+            ('status', '=', 'ACTIVE'),           # Estado igual a 'ACTIVE'
+            ('planName', '!=', 'FREE'),         # Plan distinto de 'FREE'
+            ('skuName', 'not ilike', '%Staff%')  # skuName no contiene 'Staff' en ninguna parte
+        ])
         # Escribir datos
         for row_num, subscription in enumerate(visible_subscriptions, start=1):
-
-            odooData={
-                'name': [],
-                'plan': [],
-                'partner': [],
-                'googleDiscount': [],
-                'price' : [],
-                'payment': [],
-                'invoice' : [],
-                'invoicePay' : []
-            }
 
             related_partner_name = subscription.related_partner_names #Nombre del cliente de la suscripción
             reseller_partner = self.env['reseller.partner'].search([
                 ('org_display_name', '=', related_partner_name)
             ], limit=1)  #consulta el reseller_partner q coincide con el nombre
             partner_domain = reseller_partner.domain
+            # print(partner_domain)
+            result={}
             if partner_domain:
                 # Limpiar "http://" o "https://" del dominio
                 cleaned_domain = re.sub(r'^(https?://)?(www\.)?', '', partner_domain)
                 cleaned_domain = cleaned_domain.rstrip('/')
                 
                 # Buscar el registro en sale.order donde el campo website coincida                
+                # sale_orders = self.env['sale.order'].search([
+                #     ('partner_id.website', 'ilike', '%'+cleaned_domain),  # 'ilike' para coincidencia sin distinción de mayúsculas
+                #     ('is_subscription', '=', True),  # Filtro adicional para is_subscription
+                #     ('recurring_live', '=', True),  # Filtro adicional para estado en progreso
+                #     ('order_line.product_id.product_tmpl_id.x_studio_sku', '=', subscription.skuName),  # Filtro adicional para SKU
+                #     ('order_line.product_uom_qty', '=', subscription.numberOfSeats)  # Filtro adicional para SKU
+                # ], limit=1)
+
+                # Buscar las órdenes de venta de contactos con el dominio especificado
                 sale_orders = self.env['sale.order'].search([
-                    ('partner_id.website', 'ilike', cleaned_domain),  # 'ilike' para coincidencia sin distinción de mayúsculas
-                    ('is_subscription', '=', True),  # Filtro adicional para is_subscription
-                    ('recurring_live', '=', True),  # Filtro adicional para estado en progreso
-                    ('order_line.product_id.product_tmpl_id.x_studio_sku', '=', subscription.skuName),  # Filtro adicional para SKU
-                    ('order_line.product_uom_qty', '=', subscription.numberOfSeats)  # Filtro adicional para SKU
-                ], limit=1)
- 
+                    ('partner_id.website', 'ilike', f"%{cleaned_domain}"),  # Filtro por dominio del cliente
+                ])      
+                result['partner_name'] = sale_orders.partner_id.name
+                # Si no se encuentran órdenes de venta, terminar
+                if sale_orders:
+                    matching_sale_orders = sale_orders.filtered(
+                        lambda order: any(
+                            line.product_id.product_tmpl_id.x_studio_sku == subscription.skuName
+                            for line in order.order_line
+                        )
+                    )
+                    if matching_sale_orders:
+                        # Si se encuentran órdenes de venta, buscar la primera que coincida con sku
+                        latest_sale_order = matching_sale_orders.sorted(key=lambda inv: inv.write_date or inv.create_date, reverse=True)[0]
+                        SOproduct = self.env['sale.order.line'].search([
+                            ('order_id', '=', latest_sale_order.id),  # Relacionar facturas con las órdenes de venta
+                            ('product_id.product_tmpl_id.x_studio_sku', '=', subscription.skuName)
+                        ],limit=1)
+                        result['invoice_subscription'] = SOproduct.name
+                        result['school_partner'] = latest_sale_order.user_id.partner_id.name
+                    # Buscar las facturas relacionadas con las órdenes de venta
+                    invoices = self.env['account.move'].search([
+                        ('invoice_origin', 'in', sale_orders.mapped('name')),  # Relacionar facturas con las órdenes de venta
+                        ('state', '=', 'posted')
+                    ])
+
+                    # Si no se encuentran facturas, terminar
+                    if invoices:
+
+                        # Filtrar las facturas que contengan el producto con el SKU especificado
+                        matching_invoices = invoices.filtered(
+                            lambda inv: any(
+                                line.product_id.product_tmpl_id.x_studio_sku == subscription.skuName
+                                for line in inv.invoice_line_ids
+                            )
+                        )
+                        # Si no se encuentran facturas que coincidan con el SKU, terminar
+                        if  matching_invoices:
+                            # Obtener la factura más reciente
+                            latest_invoice = matching_invoices.sorted(key=lambda inv: inv.invoice_date or inv.create_date, reverse=True)[0]
+                            
+                            invoice_state_mapping = {
+                                "posted" : "En proceso de pago",
+                                "draft"  : "Borrador",
+                                "cancel" : "Cancelada"
+                            }
+                            product = self.env['account.move.line'].search([
+                                ('move_id', '=', latest_invoice.id),  # Relacionar facturas con las órdenes de venta
+                                ('product_id.product_tmpl_id.x_studio_sku', '=', subscription.skuName),  # Filtrar facturas publicadas
+                            ],limit=1)
+
+                            so=self.env['sale.order'].search([
+                                ('name', '=', latest_invoice.invoice_origin),  # Relacionar facturas con las órdenes de venta
+                            ],limit=1)
+                            # Obtener los datos necesarios
+                            result['price'] = product.price_unit
+                            result['discount'] = product.discount
+                            result['unit_price'] = (product.price_unit * (1 - (product.discount / 100)))
+                            result['total'] = product.price_total
+                            result['recurrence'] = so.recurrence_id.name
+                            result['invoice_name'] = latest_invoice.name
+                            result['invoice_state'] = invoice_state_mapping.get(latest_invoice.state, "")
+                            
+                    else:
+                        result['price'] = SOproduct.price_unit
+                        result['discount'] = SOproduct.discount
+                        result['unit_price'] = SOproduct.price_unit
+                        result['total'] = SOproduct.price_total
+                        result['recurrence'] = latest_sale_order.recurrence_id.name
+                        result['invoice_name'] = latest_sale_order.name
+                        result['invoice_state'] = ""
+            recurrence_mapping = {
+                "Yearly" : "Anual",
+                "Monthly"  : "Mensual",
+                "Daily" : "Diario",
+                "Weekly"  : "Semanal",
+                "3 years"  : "3 Años",
+                "5 Years"  : "5 Años",
+                "Quarterly"  : "Trimestral",
+                "6 month"  : "Semestral",
+                "12 month"  : "Anual",
+            }
+            
             #data calculada o procesada
             if subscription.endTime:
                 endTime = int(subscription.endTime) / 1000
@@ -216,60 +295,41 @@ class ResellerSubscription(models.Model):
                 "ACTIVE": "Activo",
                 "SUSPENDED": "Suspendido",
             }
-            translated_plan_name = plan_name_mapping.get(subscription.planName, "Desconocido")  # Usa "Desconocido" si el valor no está en el diccionario
-            translated_status_name = status_name_mapping.get(subscription.status, "Desconocido")  # Usa "Desconocido" si el valor no está en el diccionario
-            invoice= self.env['account.move'].search([
-                ('invoice_origin', 'like',sale_orders.name)],
-                limit=1
-            )
 
             partner = self.env['reseller.partner'].search(
                 [('cloud_identity_id', 'like', subscription.customerId)],
                 limit=1
             )
 
-            # Inicializamos el valor que vamos a buscar
-            matching_order_line = None
-
-            # Iteramos sobre las líneas de pedido
-            for order_line in sale_orders.order_line:
-                # Obtenemos el producto relacionado a esta línea de pedido
-                product_template = order_line.product_id.product_tmpl_id
-
-                # Comprobamos si el SKU coincide con el de la suscripción
-                if product_template.x_studio_sku == subscription.skuName:
-                    matching_order_line = order_line
-                    break  # Salimos del bucle al encontrar el primer match
-
             worksheet.write(row_num, 0, partner.region_code) #País #ToDo obtener de consola o de odoo?
             worksheet.write(row_num, 1, "") #Unidad moneda #ToDo
             worksheet.write(row_num, 2, cleaned_domain) #Cliente dominio
-            worksheet.write(row_num, 3, sale_orders.partner_id.name or "") #Nombre Comercial 
-            worksheet.write(row_num, 4, matching_order_line.name if matching_order_line else "") #Suscripción en odoo
+            worksheet.write(row_num, 3, result.get('partner_name', '') or "")  # Nombre Comercial
+            worksheet.write(row_num, 4, result.get('invoice_subscription', '') if result else "")  # Suscripción en odoo
             worksheet.write(row_num, 5, "") #Envio de correo electronico #ToDo
             worksheet.write(row_num, 6, "") #Fecha sku anterior #ToDo
             worksheet.write(row_num, 7, "") #sku anterior
-            worksheet.write(row_num, 8, sale_orders.user_id.partner_id.name or "") #school partner
+            worksheet.write(row_num, 8, result.get('school_partner', '') if result else "")  # school partner
             worksheet.write(row_num, 9, subscription.skuName) #sku actual
-            worksheet.write(row_num, 10, translated_plan_name) #plan de pagos
-            worksheet.write(row_num, 11, translated_status_name)  #estado de la suscripcion
+            worksheet.write(row_num, 10, plan_name_mapping.get(subscription.planName, "Desconocido")) #plan de pagos
+            worksheet.write(row_num, 11, status_name_mapping.get(subscription.status, "Desconocido"))  #estado de la suscripcion
             worksheet.write(row_num, 12, day_of_ending) #dia
             worksheet.write(row_num, 13, month_of_ending) #mes
             worksheet.write(row_num, 14, year_of_ending) #año
             worksheet.write(row_num, 15, subscription.licensedNumberOfSeats) #licencias asignadas
             worksheet.write(row_num, 16, subscription.numberOfSeats) #licencias a renovar
-            worksheet.write(row_num, 17, matching_order_line.price_unit if matching_order_line else "") #precio unitario oficial google para el cliente 
-            worksheet.write(row_num, 18, matching_order_line.discount if matching_order_line else "")  #descuento google para el cliente
-            worksheet.write(row_num, 19, matching_order_line.price_reduce if matching_order_line else "") #precio unitario al publico con descuento
-            worksheet.write(row_num, 20, matching_order_line.price_subtotal if matching_order_line else "") #monto mxn a cobrar odoo sin iva
-            worksheet.write(row_num, 21, sale_orders.recurrence_id.name or "") #Cliente paga
+            worksheet.write(row_num, 17, result.get('price', '') if result else "") # precio unitario oficial google para el cliente
+            worksheet.write(row_num, 18, result.get('discount', '') if result else "") # descuento google para el cliente
+            worksheet.write(row_num, 19, result.get('unit_price', '') if result else "") # precio unitario al publico con descuento
+            worksheet.write(row_num, 20, result.get('total', '') if result else "") # monto mxn a cobrar odoo sin iva
+            worksheet.write(row_num, 21, recurrence_mapping.get(result.get('recurrence', '') or ""), "") # Cliente paga
             worksheet.write(row_num, 22, "") #costo unitario licencia consola anual
             worksheet.write(row_num, 23, "") #costo unitario licencia consola mensual
             worksheet.write(row_num, 24, "") #Monto a pagar a Google
             worksheet.write(row_num, 25, "") #Ganancia
             worksheet.write(row_num, 26, "") #Margen
-            worksheet.write(row_num, 27, invoice.name or "") #factura
-            worksheet.write(row_num, 28, "") #pago de la factura
+            worksheet.write(row_num, 27, result.get('invoice_name', '') if result else "")  # factura
+            worksheet.write(row_num, 28, result.get('invoice_state', '') if result else "")  # pago de la factura
             worksheet.write(row_num, 29, "") #Comentario upsell
             worksheet.write(row_num, 30, "") #Partner Advantage DR
             worksheet.write(row_num, 31, "") #como se paga a google
